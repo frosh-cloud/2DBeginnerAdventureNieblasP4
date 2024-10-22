05dd066d5a624644ecb548b498740b81dd143348
frosh-cloud <757085@student.dusd.net>
2024-10-22 11:45:13 -0700

    tiles created
    i finally created my tiles to decorate and  create the world of my game

--- 2DBegginerAdventureP4Nieblas/Assets/Scripts 1/PlayerController.cs
+++ 2DBegginerAdventureP4Nieblas/Assets/Scripts 1/PlayerController.cs
@@ -8,13 +8,19 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
 }
