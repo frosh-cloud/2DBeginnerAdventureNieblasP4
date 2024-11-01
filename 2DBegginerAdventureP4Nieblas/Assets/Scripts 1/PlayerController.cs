d5ded3522ed9edd139cf04ca74c0460550db602a
frosh-cloud <757085@student.dusd.net>
2024-11-01 11:44:17 -0700

    Added a health pack to my project.
    Added a new health pack to my 2D begginer adventure project so my player can have health.

--- 2DBegginerAdventureP4Nieblas/Assets/Scripts 1/PlayerController.cs
+++ 2DBegginerAdventureP4Nieblas/Assets/Scripts 1/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
@@ -12,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
         
         
     }
@@ -24,12 +30,17 @@ public class PlayerController : MonoBehaviour
 
         
     }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
-        position.x = position.x + 3.0f *horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed *horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2D.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
